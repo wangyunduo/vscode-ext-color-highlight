7fa4482fedc9f0c483429626092322a7b91ca362
Yunduo Wang <wangyunduochn@gmail.com>
2023-08-20 10:46:33 +0800

    refactor: coupling findPattern() into find()
    * currently supports hsl-level 4, hwb
    * eg: findHwb(text) -> find('hwb', text)

--- src/strategies/functions.js
+++ src/strategies/functions.js
@@ -1,3 +1,5 @@
+import { hwbPattern, hslLvl4Pattern } from './regexPatterns/colorPatterns.mjs';
+
 const colorFunctions =
   /((rgb|hsl)a?\(\s*[\d]{1,3}%?\s*(?<commaOrSpace>\s|,)\s*[\d]{1,3}%?\s*\k<commaOrSpace>\s*[\d]{1,3}%?(\s*(\k<commaOrSpace>|\/)\s*\d?\.?\d+%?)?\s*\))/gi;
 
@@ -41,4 +43,47 @@ export function sortStringsDesc(arr) {
       return 0;
     }
   });
+}
+
+/**
+ * @exports
+ * @param {string} patternName
+ * @param {string} text
+ * @returns {{
+ *  start: number,
+ *  end: number,
+ *  color: string
+ * }}
+ */
+export async function find(patternName, text) {
+  let regExp;
+  switch (patternName) {
+    case 'hsl': {
+      regExp = new RegExp(hslLvl4Pattern, 'gi');
+      break;
+    }
+    case 'hwb': {
+      regExp = new RegExp(hwbPattern, 'gi');
+      break;
+    }
+  }
+
+  let match = regExp.exec(text);
+  let result = [];
+
+  while (match !== null) {
+    const start = match.index;
+    const end = regExp.lastIndex;
+    const color = match[0];
+
+    result.push({
+      start,
+      end,
+      color,
+    });
+
+    match = regExp.exec(text);
+  }
+
+  return result;
 }
