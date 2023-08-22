1f535889d5a5434b82f9718b86f4d5325dd519bb
Yunduo Wang <wangyunduochn@gmail.com>
2023-08-22 16:51:23 +0800

    feat(no func): rgb finders

--- src/strategies/specialColorFindFunctions/rgbWithoutFunction.js
+++ src/strategies/specialColorFindFunctions/rgbWithoutFunction.js
@@ -1,11 +1,11 @@
-import Color from 'color';
+import { noRgbPatterns } from '../regexPatterns/colorPatterns.mjs';
 
-// Using [^\S\n] to avoid matching colors between lines
-const colorRgb =
-  /(\d{1,3})[^\S\n]*(?<commaOrSpace>[^\S\n]|,)[^\S\n]*(\d{1,3})[^\S\n]*\k<commaOrSpace>[^\S\n]*(\d{1,3})/g;
+export function getRgbNoFnFinders() {
+  return noRgbPatterns.map(pattern => text => findRgbNoFn(pattern, text));
+}
 
 /**
- * @export
+ * @param {RegExp} pattern
  * @param {string} text
  * @returns {{
  *  start: number,
@@ -13,30 +13,23 @@ const colorRgb =
  *  color: string
  * }}
  */
-export async function findRgbNoFn(text) {
-  let match = colorRgb.exec(text);
+async function findRgbNoFn(pattern, text) {
+  const regExp = new RegExp(pattern, 'gi');
+  let match = regExp.exec(text);
   let result = [];
 
   while (match !== null) {
-    const [matchedColor, red, , green, blue] = match;
-    const start = match.index + (match[0].length - matchedColor.length);
-    const end = colorRgb.lastIndex;
+    const start = match.index;
+    const end = regExp.lastIndex;
+    const color = 'rgb(' + match[0] + ')';
 
-    // console.log(match);
+    result.push({
+      start,
+      end,
+      color,
+    });
 
-    try {
-      const color = Color.rgb(parseInt(red), parseInt(green), parseInt(blue)).string();
-
-      result.push({
-        start,
-        end,
-        color,
-      });
-    } catch (e) {
-      console.error(e);
-    }
-
-    match = colorRgb.exec(text);
+    match = regExp.exec(text);
   }
 
   return result;
