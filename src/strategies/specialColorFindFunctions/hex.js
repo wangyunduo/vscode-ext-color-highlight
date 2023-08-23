420baebb7634c62bade2969099fed1afcff939fd
Yunduo Wang <wangyunduochn@gmail.com>
2023-08-23 10:57:15 +0800

    fix(hex): full support RGB, RGBA, RRGGBB, RRGGBBAA

--- src/strategies/specialColorFindFunctions/hex.js
+++ src/strategies/specialColorFindFunctions/hex.js
@@ -1,7 +1,12 @@
-import Color from 'color';
-
-const colorHex =
-  /.?((?:\#|\b0x)([a-f0-9]{6}([a-f0-9]{2})?|[a-f0-9]{3}([a-f0-9]{1})?))\b/gi;
+/**
+ * match[]
+ * 0: .#RRGGBB, .#RGB, .#RRGGBBAA, .#RGBA
+ * 1: #RRGGBB, #RGB, #RRGGBBAA, #RGBA
+ * 2: RRGGBB, RGB, RRGGBBAA, RGBA
+ * 3: AA
+ * 4: A
+ */
+const hexPattern = /.?((?:\#|\b0x)([a-f0-9]{6}([a-f0-9]{2})?|[a-f0-9]{3}([a-f0-9]{1})?))\b/gi;
 
 /**
  * @export
@@ -13,39 +18,26 @@ const colorHex =
  * }}
  */
 function findHex(text, useARGB) {
-  let match = colorHex.exec(text);
+  let match = hexPattern.exec(text);
   let result = [];
 
   while (match !== null) {
     const firstChar = match[0][0];
     const matchedColor = match[1];
     const start = match.index + (match[0].length - matchedColor.length);
-    const end = colorHex.lastIndex;
-    let matchedHex = '#' + match[2];
+    const end = hexPattern.lastIndex;
 
     // Check the symbol before the color match, and try to avoid coloring in the
     // contexts that are not relevant
     // https://github.com/sergiirocks/vscode-ext-color-highlight/issues/25
     if (firstChar.length && /\w/.test(firstChar)) {
-      match = colorHex.exec(text);
+      match = hexPattern.exec(text);
       continue;
     }
 
     try {
-      let color;
-      if (useARGB == true) {
-        let alphaInt = 1;
-        if (match[2].length == 8) {
-          alphaInt =
-            Math.round((parseInt(match[2].substring(0, 2), 16) * 100) / 255) /
-            100; // Get first 2 characters, convert to decimal
-          matchedHex = '#' + match[2].substring(2);
-        }
-
-        color = Color(matchedHex).alpha(alphaInt).rgb().string();
-      } else {
-        color = Color(matchedHex).rgb().string();
-      }
+      // '#' + match[2] === match[1]
+      const color = hexToRgb(match[2], useARGB);
 
       result.push({
         start,
@@ -54,7 +46,7 @@ function findHex(text, useARGB) {
       });
     } catch (e) {}
 
-    match = colorHex.exec(text);
+    match = hexPattern.exec(text);
   }
 
   return result;
@@ -84,4 +76,37 @@ export async function findHexARGB(text) {
  */
 export async function findHexRGBA(text) {
   return findHex(text, false);
+}
+
+/**
+ * * convert hex color string to rgb color string
+ * * https://www.w3.org/TR/css-color-4/#hex-notation
+ * @param {string} string - accepts RGB, RGBA, RRGGBB, RRGGBBAA without leading '#', '0x'
+ * @param {boolean} argb - if true, treats RGBA, RRGGBBAA as ARGB, AARRGGBB
+ * @returns rgb color string in rgb(r g b / a) format
+ */
+function hexToRgb(string, argb) {
+  let rgbColor = string;
+  if (string.length < 6) {
+    // convert RGB, RGBA to RRGGBB, RRGGBBAA
+    rgbColor = '';
+    for (const char of string) {
+      rgbColor += char + char;
+    }
+  }
+
+  // convert ARGB to RGBA
+  if (argb && rgbColor.length === 8) {
+    rgbColor = rgbColor.substring(2, 8) + rgbColor.substring(0, 2);
+  }
+  // console.log(string, rgbaColor);
+
+  // convert RRGGBB / RRGGBBAA to rgb(r g b / a)
+  const r = parseInt(rgbColor.substring(0, 2), 16);
+  const g = parseInt(rgbColor.substring(2, 4), 16);
+  const b = parseInt(rgbColor.substring(4, 6), 16);
+  let a = 1;
+
+  if (rgbColor.length === 8) a = parseInt(rgbColor.substring(6, 8), 16) / 0xff;
+  return `rgb(${r} ${g} ${b} / ${a})`;
 }
